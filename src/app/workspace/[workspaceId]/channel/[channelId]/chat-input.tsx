10d5f35e1642c31bbad8978c7d5c140fbee7aea4
Prachi Kushwaha <prachiku673@gmail.com>
2024-10-13 14:43:02 +0530

    editor submit functionality

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -1,24 +1,57 @@
-import dynamic from "next/dynamic"
-import Quill from "quill"
-import { useRef } from "react"
+import { useCreateMessage } from "@/features/messages/api/use-create-message";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { useChannelId } from "@/hooks/use-channel-id";
+import dynamic from "next/dynamic";
+import Quill from "quill";
+import { useRef, useState } from "react";
+import { toast } from "sonner";
 
-const Editor = dynamic(() => import("@/components/editor"), {ssr:false})    
+const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
 
-interface ChatInputProps{
-    placeholder?:string
+interface ChatInputProps {
+  placeholder?: string;
 }
 
-export const ChatInput = ({placeholder}:ChatInputProps)=>{
-const editorRef = useRef<Quill|null>(null)
+export const ChatInput = ({ placeholder }: ChatInputProps) =>{
+  const editorRef = useRef<Quill | null>(null);
+  const [editorKey, setEditorKey] = useState(0)
+  const [isPending, setIsPending] = useState(false)
 
-    return (
-        <div className="px-5 w-full">
-            <Editor 
-              placeholder={placeholder}
-              onSubmit={()=>{}}
-              disabled={false}
-              innerRef={editorRef}
-            />
-        </div>
-    )
+  const workspaceId = useWorkspaceId()
+  const channelId = useChannelId()
+  const {mutate:createMessage} = useCreateMessage()
+
+  const handleSubmit = ({
+    body,
+    image,
+  }: {
+    body: string;
+    image: File | null;
+  }) => {
+    console.log({ body, image });
+    try {
+      createMessage({
+        workspaceId,
+        channelId,
+        body,
+      }, {throwError:true})
+   setEditorKey((prevKey)=>prevKey+1)
+    } catch (error) {
+      toast.error("failed to send messages")
+    } finally{
+      setIsPending(false)
+    }
+  }  
+
+  return (
+    <div className="px-5 w-full">
+      <Editor
+      key={editorKey}
+        placeholder={placeholder}
+        onSubmit={handleSubmit}
+        disabled={isPending}
+        innerRef={editorRef}
+      />
+    </div>
+  );
 }
--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -2,7 +2,7 @@ import Quill, { type QuillOptions } from "quill";
 import "quill/dist/quill.snow.css";
 import { PiTextAa } from "react-icons/pi";
 import { MdSend } from "react-icons/md";
-import Image from "next/image"
+import Image from "next/image";
 import {
   MutableRefObject,
   useEffect,
@@ -43,7 +43,7 @@ const Editor = ({
   innerRef,
 }: EditorProps) => {
   const [text, setText] = useState("");
-  const [image, setImage] = useState<File| null>(null)
+  const [image, setImage] = useState<File | null>(null);
   const [isToolbarVisible, setIsToolbarVisible] = useState(true);
 
   const containerRef = useRef<HTMLDivElement>(null);
@@ -52,7 +52,7 @@ const Editor = ({
   const quillRef = useRef<Quill | null>(null);
   const defaultValueRef = useRef(defaultValue);
   const disabledRef = useRef(disabled);
-  const imageElementRef = useRef<HTMLInputElement>(null)
+  const imageElementRef = useRef<HTMLInputElement>(null);
 
   useLayoutEffect(() => {
     submitRef.current = onSubmit;
@@ -85,7 +85,16 @@ const Editor = ({
             enter: {
               key: "Enter",
               handler: () => {
-                return;
+                const text = quill.getText();
+                const addedImage = imageElementRef.current?.files?.[0] || null;
+
+                const isEmpty =
+                  !addedImage &&
+                  text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+                if (isEmpty) return;
+
+                const body = JSON.stringify(quill.getContents());
+                submitRef.current?.({ body, image:addedImage });
               },
             },
           },
@@ -141,45 +150,45 @@ const Editor = ({
   const onEmojiSelect = (emoji: any) => {
     const quill = quillRef.current;
 
-    quill?.insertText(quill?.getSelection()?.index || 0, emoji.native)
+    quill?.insertText(quill?.getSelection()?.index || 0, emoji.native);
   };
 
-  const isEmpty = text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+  const isEmpty = !image && text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
 
   return (
     <div className="flex flex-col">
-      <input 
+      <input
         type="file"
         accept="image/*"
         ref={imageElementRef}
-        onChange={(event)=>setImage(event?.target.files![0])}
+        onChange={(event) => setImage(event?.target.files![0])}
         className="hidden"
-
       />
-      <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
+      <div className={cn("flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white", 
+        disabled && "opacity-50"
+      )}>
         <div ref={containerRef} className="h-full ql-custom" />
         {!!image && (
           <div className="p-2">
-           <div className="relative size-[62px] flex items-center justify-center group/image">
-           <Hint label="remove image">
-
-             <button
-                onClick={()=>{
-                  setImage(null)
-                  imageElementRef.current!.value = ""
-                }}
-                className="hidden group-hover/image:flex rounded-full bg-black/70 hover:bg-black absolute -top-2.5 -right-2.5 text-white size-6 z-[4] border-2 border-white items-center justify-center"
-              >
-             <XIcon className="size-3.5"/>
-             </button>
-           </Hint>
-             <Image
-              src={URL.createObjectURL(image)}
-              alt="Uploaded"
-              fill
-              className="rounded-xl overflow-hidden border object-cover"
-             />
-           </div>
+            <div className="relative size-[62px] flex items-center justify-center group/image">
+              <Hint label="remove image">
+                <button
+                  onClick={() => {
+                    setImage(null);
+                    imageElementRef.current!.value = "";
+                  }}
+                  className="hidden group-hover/image:flex rounded-full bg-black/70 hover:bg-black absolute -top-2.5 -right-2.5 text-white size-6 z-[4] border-2 border-white items-center justify-center"
+                >
+                  <XIcon className="size-3.5" />
+                </button>
+              </Hint>
+              <Image
+                src={URL.createObjectURL(image)}
+                alt="Uploaded"
+                fill
+                className="rounded-xl overflow-hidden border object-cover"
+              />
+            </div>
           </div>
         )}
         <div className="flex px-2 pb-2 z-[5]">
@@ -217,14 +226,19 @@ const Editor = ({
               <Button
                 variant="outline"
                 size="sm"
-                onClick={() => {}}
+                onClick={onCancel}
                 disabled={disabled}
               >
                 Cancel
               </Button>
               <Button
                 disabled={disabled}
-                onClick={() => {}}
+                onClick={() => {
+                  onSubmit({
+                    body: JSON.stringify(quillRef.current?.getContents()),
+                    image,
+                  });
+                }}
                 size="sm"
                 className=" bg-[#007a5a] hover:bg-[#007a5a]/80 text-white"
               >
@@ -235,7 +249,12 @@ const Editor = ({
           {variant === "create" && (
             <Button
               disabled={disabled || isEmpty}
-              onClick={() => {}}
+              onClick={() => {
+                onSubmit({
+                  body: JSON.stringify(quillRef.current?.getContents()),
+                  image,
+                });
+              }}
               size="iconSm"
               className={cn(
                 "ml-auto",
