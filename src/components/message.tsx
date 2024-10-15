6277a62a11c45a2bb2f42afe37134d08985dcf54
Prachi Kushwaha <prachiku673@gmail.com>
2024-10-15 11:41:08 +0530

    mesage toolbar component

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -4,6 +4,7 @@ import dynamic from "next/dynamic";
 import { Hint } from "./hint";
 import { AvatarImage, Avatar, AvatarFallback } from "./ui/avatar";
 import { Thumbnail } from "./thumbnail";
+import { Toolbar } from "./toolbar";
 
 const Renderer = dynamic(() => import("@/components/renderer"), { ssr: false });
 
@@ -108,6 +109,18 @@ export const Message = ({
           ) : null}
         </div>
       </div>
+      {!isEditing && (
+        <Toolbar
+          isAuthor = {isAuthor}
+          isPending={false}
+          handleEdit ={()=>setEditingId(id)}
+          handleThread = {()=>{}}
+          handleDelete={()=>{}}
+          handleReaction={()=>{}}
+          hideThreadButton={hideThreadButton}
+
+        />
+      )}
     </div>
   );
 };
---
+++ src/components/toolbar.tsx
@@ -0,0 +1,63 @@
+import { isAuthenticatedNextjs } from "@convex-dev/auth/nextjs/server";
+import { Button } from "./ui/button";
+import { MessageSquareTextIcon, Pencil, Smile, Trash } from "lucide-react";
+import { Hint } from "./hint";
+import { EmojiPopover } from "./emoji-popover";
+interface ToolbarProps {
+  isAuthor: boolean;
+  isPending: boolean;
+  handleEdit: () => void;
+  handleDelete: () => void;
+  handleThread: () => void;
+  handleReaction: (value: string) => void;
+  hideThreadButton: boolean;
+}
+
+export const Toolbar = ({
+  isAuthor,
+  isPending,
+  handleEdit,
+  handleDelete,
+  handleThread,
+  handleReaction,
+  hideThreadButton,
+}: ToolbarProps) => {
+  return (
+    <div className="absolute top-0 right-0">
+      <div className="group-hover:opacity-100 opacity-0 transition-opacity border bg-white rounded-md shadow-sm">
+        <EmojiPopover
+          hint="add reaction"
+          onEmojiSelect={(emoji) => handleReaction(emoji.native)}
+        >
+          <Button variant="ghost" size="iconSm" disabled={isPending}>
+            <Smile className="size-4" />
+          </Button>
+        </EmojiPopover>
+         {!hideThreadButton && (
+             
+        <Hint label="reply to a thread">
+          <Button variant="ghost" size="iconSm" disabled={isPending}>
+            <MessageSquareTextIcon className="size-4" />
+          </Button>
+        </Hint>
+         )}
+         {isAuthor && (
+             
+        <Hint label="edit message">
+          <Button variant="ghost" size="iconSm" disabled={isPending}>
+            <Pencil className="size-4" />
+          </Button>
+        </Hint>
+         )}
+         {isAuthor && (
+
+        <Hint label="delete message">
+          <Button variant="ghost" size="iconSm" disabled={isPending}>
+            <Trash className="size-4" />
+          </Button>
+        </Hint>
+         )}
+      </div>
+    </div>
+  );
+};
